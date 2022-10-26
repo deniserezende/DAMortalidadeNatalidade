4e2916e901fc3aeaa20a84c0194139b743312a7e
Denise Rezende <denise.rezende@gmail.com>
2022-10-26 15:41:12 -0300

    Create files to deal with the csv load (carga)

---
+++ src/main/java/dao/PgCargaDAO.java
@@ -0,0 +1,34 @@
+package dao;
+
+import model.Carga;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class PgCargaDAO extends CargaDAO{
+
+    @Override
+    public void create(Carga carga) throws SQLException {
+
+    }
+
+    @Override
+    public Carga read(Integer id) throws SQLException {
+        return null;
+    }
+
+    @Override
+    public void update(Carga carga) throws SQLException {
+
+    }
+
+    @Override
+    public void delete(Integer id) throws SQLException {
+
+    }
+
+    @Override
+    public List<Carga> all() throws SQLException {
+        return null;
+    }
+}
