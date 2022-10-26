e27ddafbb83506922eda0d76132295071f99dfb2
Denise Rezende <denise.rezende@gmail.com>
2022-10-25 22:14:42 -0300

    Add object Registro

---
+++ src/main/java/model/Registro.java
@@ -0,0 +1,22 @@
+package model;
+
+public class Registro {
+    private Integer id_registro;
+    private String tipo_registro;
+
+    public Integer getId_registro() {
+        return id_registro;
+    }
+
+    public void setId_registro(Integer id_registro) {
+        this.id_registro = id_registro;
+    }
+
+    public String getTipo_registro() {
+        return tipo_registro;
+    }
+
+    public void setTipo_registro(String tipo_registro) {
+        this.tipo_registro = tipo_registro;
+    }
+}
