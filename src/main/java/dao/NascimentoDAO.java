29801de5b989f1006c314ee7e92b60daf8ddbe4c
Laura Rocha <laura13122001@gmail.com>
2022-10-31 09:01:50 -0300

    Create NascimentoDAO.java

--- src/main/java/dao/NascimentoDAO.java
+++ src/main/java/dao/NascimentoDAO.java
@@ -1,4 +1,8 @@
 package dao;
 
-public interface NascimentoDAO {
+import model.Registrado;
+
+public interface NascimentoDAO extends DAO<Registrado> {
+//    public void authenticate(User usuario) throws SQLException, SecurityException;
+//    public User getByLogin(String login) throws SQLException;
 }
