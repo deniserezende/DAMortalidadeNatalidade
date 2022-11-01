af66a12984b3decf5927cb1645c4b5a2fc80a1dd
Denise Rezende <denise.rezende@gmail.com>
2022-10-31 21:48:28 -0300

    Delete NascimentoDAO.java

--- src/main/java/dao/NascimentoDAO.java
+++
@@ -1,8 +0,0 @@
-package dao;
-
-import model.Registrado;
-
-public interface NascimentoDAO extends DAO<Registrado> {
-//    public void authenticate(User usuario) throws SQLException, SecurityException;
-//    public User getByLogin(String login) throws SQLException;
-}
