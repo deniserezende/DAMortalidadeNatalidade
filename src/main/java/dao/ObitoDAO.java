a7796fabb53e73a915b3d88e4f11075d85d6c1f8
Laura Rocha <laura13122001@gmail.com>
2022-11-01 13:32:36 -0300

    Delete trash files

--- src/main/java/dao/ObitoDAO.java
+++
@@ -1,8 +0,0 @@
-package dao;
-
-import model.Registrado;
-
-public interface ObitoDAO extends DAO<Registrado> {
-//    public void authenticate(User usuario) throws SQLException, SecurityException;
-//    public User getByLogin(String login) throws SQLException;
-}
