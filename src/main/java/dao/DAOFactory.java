0822abaa879ae68e0fc2db1c5c5979936fa9f107
Denise Rezende <denise.rezende@gmail.com>
2022-11-03 13:07:51 -0300

    Comment unused code*
    * Check if Transaction functions are useful in this application

--- src/main/java/dao/DAOFactory.java
+++ src/main/java/dao/DAOFactory.java
@@ -28,61 +28,61 @@ public abstract class DAOFactory implements AutoCloseable{
     }
 
     // TODO ver se todas essas funções são úteis.
-
-    public void beginTransaction() throws SQLException {
-        try {
-            connection.setAutoCommit(false);
-        } catch (SQLException error) {
-            logger.error("beginTransaction: " + error);
-
-            System.err.println(error.getMessage());
-            throw new SQLException("Erro ao abrir transação.");
-        }
-    }
-
-    public void commitTransaction() throws SQLException {
-        try {
-            connection.commit();
-        } catch (SQLException error) {
-            logger.error("commitTransaction: " + error);
-
-            System.err.println(error.getMessage());
-            throw new SQLException("Erro ao finalizar transação.");
-        }
-    }
-
-    public void rollbackTransaction() throws SQLException {
-        try {
-            connection.rollback();
-        } catch (SQLException error) {
-            logger.error("rollbackTransaction: " + error);
-
-            System.err.println(error.getMessage());
-            throw new SQLException("Erro ao executar transação.");
-        }
-    }
-
-    public void endTransaction() throws SQLException {
-        try {
-            connection.setAutoCommit(true);
-        } catch (SQLException error) {
-            logger.error("endTransaction: " + error);
-
-            System.err.println(error.getMessage());
-            throw new SQLException("Erro ao finalizar transação.");
-        }
-    }
-
-    public void closeConnection() throws SQLException {
-        try {
-            connection.close();
-        } catch (SQLException error) {
-            logger.error("closeConnection: " + error);
-
-            System.err.println(error.getMessage());
-            throw new SQLException("Erro ao fechar conexão ao banco de dados.");
-        }
-    }
+//
+//    public void beginTransaction() throws SQLException {
+//        try {
+//            connection.setAutoCommit(false);
+//        } catch (SQLException error) {
+//            logger.error("beginTransaction: " + error);
+//
+//            System.err.println(error.getMessage());
+//            throw new SQLException("Erro ao abrir transação.");
+//        }
+//    }
+//
+//    public void commitTransaction() throws SQLException {
+//        try {
+//            connection.commit();
+//        } catch (SQLException error) {
+//            logger.error("commitTransaction: " + error);
+//
+//            System.err.println(error.getMessage());
+//            throw new SQLException("Erro ao finalizar transação.");
+//        }
+//    }
+//
+//    public void rollbackTransaction() throws SQLException {
+//        try {
+//            connection.rollback();
+//        } catch (SQLException error) {
+//            logger.error("rollbackTransaction: " + error);
+//
+//            System.err.println(error.getMessage());
+//            throw new SQLException("Erro ao executar transação.");
+//        }
+//    }
+//
+//    public void endTransaction() throws SQLException {
+//        try {
+//            connection.setAutoCommit(true);
+//        } catch (SQLException error) {
+//            logger.error("endTransaction: " + error);
+//
+//            System.err.println(error.getMessage());
+//            throw new SQLException("Erro ao finalizar transação.");
+//        }
+//    }
+//
+//    public void closeConnection() throws SQLException {
+//        try {
+//            connection.close();
+//        } catch (SQLException error) {
+//            logger.error("closeConnection: " + error);
+//
+//            System.err.println(error.getMessage());
+//            throw new SQLException("Erro ao fechar conexão ao banco de dados.");
+//        }
+//    }
 
     public abstract CargaDAO getCargaDAO();
     public abstract RegistradoDAO getRegistradoDAO();
